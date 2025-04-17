dae6ab30afc7d734d5b3d776e2687c3c96b64436
rc1405 <minervaconsole@gmail.com>
2025-04-16 22:06:10 -0500

    fmt and docs

--- cmd/src/main.rs
+++ cmd/src/main.rs
@@ -1,4 +1,8 @@
-//! Some Crate level documentation to make the linter happy
+//! Fast and flexible data stream processor written in Rust
+//!
+//! Provides a cli for running, linting and testing data streaming pipelines 
+//! using a declaritive yaml based configuration for data aggregation and
+//! transformation
 use clap::{Args, Parser};
 use futures::stream::FuturesOrdered;
 use futures::stream::StreamExt;
