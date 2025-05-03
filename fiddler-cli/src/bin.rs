59ddb180512fe74609f0dc65f7f46f21f8dfb6c8
rc1405 <minervaconsole@gmail.com>
2025-05-02 22:25:04 -0500

    fmt

--- fiddler-cli/src/bin.rs
+++ fiddler-cli/src/bin.rs
@@ -3,8 +3,8 @@
 //! Provides a cli for running, linting and testing data streaming pipelines
 //! using a declaritive yaml based configuration for data aggregation and
 //! transformation
-use fiddler_cmd::run;
 use fiddler::Error;
+use fiddler_cmd::run;
 
 #[tokio::main]
 async fn main() -> Result<(), Error> {
