9c1e9a70a80ed22662b07e7add1fb3edc224722c
rc1405 <ryancote1405@gmail.com>
2026-01-30 16:54:21 -0600

    feat: added cloudwatch plugin (#70)
    Co-authored-by: rc1405 <minervaconsole@gmail.com>

--- fiddler/src/modules/aws/mod.rs
+++ fiddler/src/modules/aws/mod.rs
@@ -1,9 +1,10 @@
 use crate::Error;
 use serde::Deserialize;
+pub mod cloudwatch;
 mod s3;
 mod sqs;
 
-#[derive(Deserialize, Clone)]
+#[derive(Debug, Deserialize, Clone)]
 pub(crate) struct Credentials {
     pub access_key_id: String,
     pub secret_access_key: String,
@@ -12,5 +13,6 @@ pub(crate) struct Credentials {
 pub(crate) fn register_plugins() -> Result<(), Error> {
     sqs::register_sqs()?;
     s3::register_s3()?;
+    cloudwatch::register_cloudwatch()?;
     Ok(())
 }
